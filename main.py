fab15bf2de1db060e86f9af3252baebc2bd05cf2
AngeloDamante <angelo.damante16@gmail.com>
2024-12-30 15:33:54 +0100

    chore: first commit to preapre for new GDR

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+A simple main to handle GDR project
+
+@Author: Vittorione
+@Date: January 2025
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+	main()
---
+++ src/Character.py
@@ -0,0 +1,6 @@
+"""
+A simple class to handle DB Characters
+
+@Author: Vittorione
+@Date: January 2025
+"""
